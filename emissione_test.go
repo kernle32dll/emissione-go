1b978465b2480b116b5665409b3a7d6844d39a83
Björn Gerdau <bjoerngerdau@gmx.de>
2019-10-13 13:53:06 +0200

    test: improve test isolation of emissione_test

--- emissione_test.go
+++ emissione_test.go
@@ -3,57 +3,98 @@ package emissione_test
 import (
 	"github.com/kernle32dll/emissione-go"
 
-	"encoding/json"
-	"encoding/xml"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+type MockWriter struct {
+	writeOutput string
+	errorOut    error
+}
+
+func (mockWriter MockWriter) Write(w http.ResponseWriter, i interface{}) error {
+	if mockWriter.writeOutput != "" {
+		_, err := w.Write([]byte(mockWriter.writeOutput))
+		return err
+	}
+
+	return mockWriter.errorOut
+}
+
 func TestHandler_Write(t *testing.T) {
-	type SampleObject struct {
-		TestProp int `json:"testProp",xml:"testProp"`
-	}
+	jsonOut, xmlOut := "json", "xml"
 
-	sampleObject := SampleObject{42}
+	jsonWriter := &MockWriter{jsonOut, nil}
+	xmlWriter := &MockWriter{xmlOut, nil}
 
-	jsonIndent, err := json.MarshalIndent(sampleObject, "", "  ")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	xmlIndent, err := xml.MarshalIndent(sampleObject, "", "  ")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	writer := emissione.Default()
+	writer := emissione.New(
+		jsonWriter,
+		emissione.WriterMapping{
+			"application/json": jsonWriter,
+			"application/xml":  xmlWriter,
+		},
+	)
 
 	tests := []struct {
 		name    string
 		accepts []string
 		want    string
 	}{
-		{"default", []string{"unknown"}, string(jsonIndent)},
-		{"empty-header", nil, string(jsonIndent)},
-		{"empty-but-different", []string{""}, string(jsonIndent)},
-		{"*/*", []string{"*/*"}, string(jsonIndent)},
-		{"application/json", []string{"application/json"}, string(jsonIndent)},
-		{"application/*", []string{"application/*"}, string(jsonIndent)},
-		{"application/xml", []string{"application/xml"}, string(xmlIndent)},
-		{"weighted", []string{"application/*, application/xml;q=2"}, string(xmlIndent)},
+		{"default", []string{"unknown"}, jsonOut},
+		{"empty-header", nil, jsonOut},
+		{"empty-but-different", []string{""}, jsonOut},
+		{"*/*", []string{"*/*"}, jsonOut},
+		{"application/json", []string{"application/json"}, jsonOut},
+		{"application/*", []string{"application/*"}, jsonOut},
+		{"application/xml", []string{"application/xml"}, xmlOut},
+		{"weighted", []string{"application/*, application/xml;q=2"}, xmlOut},
 	}
-	for _, tt := range tests {
+	for i := range tests {
+		tt := tests[i]
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
 			w := httptest.NewRecorder()
 
-			writer.Write(w, &http.Request{Header: map[string][]string{"Accept": tt.accepts}}, http.StatusTeapot, sampleObject)
+			writer.Write(w, &http.Request{Header: map[string][]string{"Accept": tt.accepts}}, http.StatusTeapot, struct{}{})
+
+			if w.Code != http.StatusTeapot {
+				t.Errorf("Write() = %v, want %v", w.Code, http.StatusTeapot)
+			}
 
 			if got := w.Body.String(); got != tt.want {
 				t.Errorf("Write() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func TestHandler_Write_error(t *testing.T) {
+	expectedErr := errors.New("some-error")
+	errorWriter := &MockWriter{"", expectedErr}
+
+	writer := emissione.New(
+		errorWriter,
+		nil,
+	)
+
+	w := httptest.NewRecorder()
+
+	// Catch panic
+	defer func() {
+		if got := w.Body.String(); got != "" {
+			t.Errorf("Write() = %v, want %v", got, "")
+		}
+
+		if r := recover(); r != nil {
+			if r != expectedErr {
+				t.Errorf("Write_error() = %v, want %v", r, expectedErr)
+			}
+		}
+	}()
+	writer.Write(w, &http.Request{}, http.StatusTeapot, struct{}{})
+
+	t.Errorf("Write_error() = %v, want %v", nil, expectedErr)
 }
