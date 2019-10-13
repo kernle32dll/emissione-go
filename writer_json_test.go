1331b0b7369f0c6b70bb9d762624bfe8a228637a
Björn Gerdau <bjoerngerdau@gmx.de>
2019-10-13 14:13:56 +0200

    test: add tests for json and xml writers

---
+++ writer_json_test.go
@@ -0,0 +1,67 @@
+package emissione_test
+
+import (
+	"github.com/kernle32dll/emissione-go"
+
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestJSONWriter(t *testing.T) {
+	type SampleObject struct {
+		TestProp int `json:"testProp"`
+	}
+
+	sampleObject := SampleObject{42}
+
+	t.Run("simple", func(t *testing.T) {
+		jsonMarshalled, mErr := json.Marshal(sampleObject)
+		if mErr != nil {
+			t.Error(mErr)
+		}
+
+		writer := emissione.NewJSONWriter()
+
+		w := httptest.NewRecorder()
+
+		if writer.Write(w, sampleObject) != nil {
+			t.Errorf("TestJSONWriter() unpexected error")
+		}
+
+		want := string(jsonMarshalled)
+		if got := w.Body.String(); got != want {
+			t.Errorf("TestJSONWriter() = %v, want %v", got, want)
+		}
+
+		wantHeader := "application/json;charset=utf-8"
+		if got := w.Header().Get("Content-Type"); got != wantHeader {
+			t.Errorf("TestJSONWriter() = %v, want %v", got, wantHeader)
+		}
+	})
+
+	t.Run("indent", func(t *testing.T) {
+		jsonMarshalled, mErr := json.MarshalIndent(sampleObject, "x", "**")
+		if mErr != nil {
+			t.Error(mErr)
+		}
+
+		writer := emissione.NewJSONIndentWriter("x", "**")
+
+		w := httptest.NewRecorder()
+
+		if writer.Write(w, sampleObject) != nil {
+			t.Errorf("TestJSONWriter() unpexected error")
+		}
+
+		want := string(jsonMarshalled)
+		if got := w.Body.String(); got != want {
+			t.Errorf("TestJSONWriter() = %v, want %v", got, want)
+		}
+
+		wantHeader := "application/json;charset=utf-8"
+		if got := w.Header().Get("Content-Type"); got != wantHeader {
+			t.Errorf("TestJSONWriter() = %v, want %v", got, wantHeader)
+		}
+	})
+}
---
+++ writer_xml_test.go
@@ -0,0 +1,67 @@
+package emissione_test
+
+import (
+	"github.com/kernle32dll/emissione-go"
+
+	"encoding/xml"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestXMLWriter(t *testing.T) {
+	type SampleObject struct {
+		TestProp int `xml:"TestProp"`
+	}
+
+	sampleObject := SampleObject{42}
+
+	t.Run("simple", func(t *testing.T) {
+		xmlMarshalled, mErr := xml.Marshal(sampleObject)
+		if mErr != nil {
+			t.Error(mErr)
+		}
+
+		writer := emissione.NewXmlWriter()
+
+		w := httptest.NewRecorder()
+
+		if writer.Write(w, sampleObject) != nil {
+			t.Errorf("TestXMLWriter() unpexected error")
+		}
+
+		want := string(xmlMarshalled)
+		if got := w.Body.String(); got != want {
+			t.Errorf("TestXMLWriter() = %v, want %v", got, want)
+		}
+
+		wantHeader := "application/xml;charset=utf-8"
+		if got := w.Header().Get("Content-Type"); got != wantHeader {
+			t.Errorf("TestXMLWriter() = %v, want %v", got, wantHeader)
+		}
+	})
+
+	t.Run("indent", func(t *testing.T) {
+		xmlMarshalled, mErr := xml.MarshalIndent(sampleObject, "x", "**")
+		if mErr != nil {
+			t.Error(mErr)
+		}
+
+		writer := emissione.NewXmlIndentWriter("x", "**")
+
+		w := httptest.NewRecorder()
+
+		if writer.Write(w, sampleObject) != nil {
+			t.Errorf("TestXMLWriter() unpexected error")
+		}
+
+		want := string(xmlMarshalled)
+		if got := w.Body.String(); got != want {
+			t.Errorf("TestXMLWriter() = %v, want %v", got, want)
+		}
+
+		wantHeader := "application/xml;charset=utf-8"
+		if got := w.Header().Get("Content-Type"); got != wantHeader {
+			t.Errorf("TestXMLWriter() = %v, want %v", got, wantHeader)
+		}
+	})
+}
